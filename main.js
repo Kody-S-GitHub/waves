6cb13bdf694055a0e49384024d6bf3262531fbb9
Kody-S-GitHub <83673803+Kody-S-GitHub@users.noreply.github.com>
2021-05-06 10:37:00 -0700

    Update main.js

--- main.js
+++ main.js
@@ -15,31 +15,37 @@ var round = true;
 function startTimer()
 {
 
-	let wait;
-	if (slowmo)
+	if (timer == undefined) 
 	{
 	
-		wait = 150;
-		
-	} else
-	{
-	
-		wait = 10;
+		let wait;
+		if (slowmo)
+		{
+
+			wait = 125;
+
+		} else
+		{
+
+			wait = 10;
+
+		}
+
+		timer = setInterval(function()
+		{
+
+			start();
+
+		}, wait);
 		
 	}
-	
-	timer = setInterval(function()
-	{
-	
-		start();
-		
-	}, wait);
 }
 
 function endTimer()
 {
 
 	clearInterval(timer);
+	timer = undefined;
 	
 }
 
@@ -63,32 +69,31 @@ function reset()
 	amplitude = document.getElementById('amplitude').value;
 	frequency = document.getElementById('frequency').value / 4;
 	
-	document.getElementById('aDisplay').innerHTML = 'Amplitude: ' + amplitude + ' Meters';
+	document.getElementById('aDisplay').innerHTML = 'Amplitude: ' + amplitude + ' Cm';
 	document.getElementById('fDisplay').innerHTML = 'Frequency: ' + frequency + ' Hz';
 	let period;
 	if (round) { period = Math.floor((360*speed)/frequency)/100; }
 	else { period = ((360*speed)/frequency)/100 }
-	document.getElementById('attributes').innerHTML = 'Crest: ' + amplitude + ' Meters, Trough: -' + amplitude + ' Meters, Period: ' + period + ' Seconds';
+	document.getElementById('attributes').innerHTML = 'Crest: ' + amplitude + ' Cm, Trough: -' + amplitude + ' Cm, Period: ' + period + ' Seconds';
 	
 }
 
-function wave()
+function sineWave()
 {
 
-	let y = (Math.sin(time * (Math.PI/180) * frequency)) * amplitude + equilibrium;
-	return y;
+	return (Math.sin(time * (Math.PI/180) * frequency)) * amplitude + equilibrium;
 	
 }
 
 function start()
 {
 
-	var y = wave();
+	var y = sineWave();
 	drawLine(time - (speed), last, time, y);
 	last = y;
 	time += speed;
 	
-	let phase = (time*frequency)%360;
+	let phase = ((time*frequency)%360).toFixed(2);
 	let percent;
 	if (phase == 0)
 	{
@@ -103,10 +108,10 @@ function start()
 	}
 	
 	let height;
-	if (round) { height = Math.floor((y - equilibrium) * -1000)/1000; }
+	if (round) { height = (Math.floor((y - equilibrium) * -1000)/1000).toFixed(3); }
 	else { height = y - equilibrium * -1 }
 	
-	document.getElementById('current').innerHTML = 'Current Height: ' + height + ' Meters, Current Time: ' + time/100 + ' Seconds, Phase: ' + phase + '/360(' + percent + '%)';
+	document.getElementById('current').innerHTML = 'Current Height: ' + height + ' Cm, Current Time: ' + (time/100).toFixed(2) + ' Seconds, Phase: ' + phase + '/360(' + percent + '%)';
 	
 }
 
@@ -128,7 +133,7 @@ function clearCanvas()
 	document.getElementById('canvas').remove();
 	let canvas = document.createElement('canvas');
 	canvas.id = 'canvas';
-	document.body.appendChild(canvas);
+	document.getElementById('canvasHolder').appendChild(canvas);
 	
 	canvas.width = window.innerWidth - 20;
 	canvas.height = 500;
@@ -136,19 +141,14 @@ function clearCanvas()
 	time = 0;
 	drawLine(0, equilibrium, canvas.width, equilibrium);
 	
-	document.getElementById('current').innerHTML = 'Current Height: 0 Meters, Current Time: 0, Phase: 0/360(0%)';
+	document.getElementById('current').innerHTML = 'Current Height: 0 Cm, Current Time: 0, Phase: 0/360(0%)';
 	
 }
 
 function slow()
 {
 
-	if (timer != undefined)
-	{
-	
-		endTimer();
-		
-	}
+	endTimer();
 	
 	let input = document.getElementById('slowmo');
 	if (!slowmo)
@@ -163,12 +163,8 @@ function slow()
 		
 	}
 		
-	if (timer != undefined)
-	{
-	
-		startTimer();
+	startTimer();
 		
-	}
 }
 
 function rounded()
@@ -189,6 +185,6 @@ function rounded()
 	let period;
 	if (round) { period = Math.floor((360*speed)/frequency)/100; }
 	else { period = ((360*speed)/frequency)/100 }
-	document.getElementById('attributes').innerHTML = 'Crest: ' + amplitude + ' Meters, Trough: -' + amplitude + ' Meters, Period: ' + period + ' Seconds';
+	document.getElementById('attributes').innerHTML = 'Crest: ' + amplitude + ' Cm, Trough: -' + amplitude + ' Cm, Period: ' + period + ' Seconds';
 
 }
